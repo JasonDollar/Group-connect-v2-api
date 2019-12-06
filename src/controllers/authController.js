227bec2c94911b3a946a535026238f3a75af707b
JasonDollar <elrey33@gmail.com>
2019-12-06 18:33:24 +0100

    fixes in getUserInfoFromCookie middleware

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -90,40 +90,40 @@ exports.protect = async (req, res, next) => {
 
 // it is like protect but only collects user info if available, no guarding
 exports.getUserInfoFromCookie = async (req, res, next) => {
-  // try {
-  let token
+  try {
+    let token
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1]
+    } else if (req.cookies.jwt) {
+      token = req.cookies.jwt
+    }
   
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    token = req.headers.authorization.split(' ')[1]
-  } else if (req.cookies.jwt) {
-    token = req.cookies.jwt
-  }
-  
-  if (!token) {
-    req.user = null
-    return next()
-  }
-    
-  const decoded = jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-    if (err) {
+    if (!token) {
       req.user = null
       return next()
     }
-    return decodedToken
-  })
     
-  const freshUser = await User.findById(decoded.id).select('-__v -createdAt')
+    const decoded = jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        req.user = null
+        return next()
+      }
+      return decodedToken
+    })
+    
+    const freshUser = await User.findById(decoded.id).select('-__v -createdAt')
   
-  if (!freshUser) {
+    if (!freshUser) {
+      req.user = null
+      return next()
+    }
+  
+    req.user = freshUser
+    next() 
+  } catch (e) {
     req.user = null
-    return next()
+    next()
   }
-  
-  req.user = freshUser
-  next() 
-  // } catch (e) {
-  //   res.status(401).send(e.message)
-  // }
 
 }
 
